ad178a21e22770a08929ccb9eac564850dac5070
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2025-02-03 00:32:10 +0500

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { productSchema } from "./products";
 import { categorySchema } from "./categories";
-
+import order from './schemas/order'
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema, categorySchema],
+  types: [productSchema, categorySchema, order],
 };
