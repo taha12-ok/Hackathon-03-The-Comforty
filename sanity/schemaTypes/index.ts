9aeea62242f50f396bf6eb07e02ecfa72f0c3e5d
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2025-02-03 01:17:58 +0500

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { productSchema } from "./products";
 import { categorySchema } from "./categories";
-import {orderSchema} from "./order";
+import {orderSchema} from "./orders";
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [productSchema, categorySchema, orderSchema],
 };
