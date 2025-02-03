6d8448f0edb9b2622e63d55419555731dfd65cb5
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2025-02-04 01:15:11 +0500

    Update middleware.ts

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,17 +1,29 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  publicRoutes: ["/", "/sign-in"],
-  afterAuth(auth, req) {
-    // Redirect authenticated users to home page if they're on a public route
-    if (auth.userId && auth.isPublicRoute) {
-      const homeUrl = new URL("/home", req.url)
-      return Response.redirect(homeUrl)
-    }
-  },
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/home', 
+  '/shop', 
+  '/about', 
+  '/contact', 
+  '/faq',
+  '/sign-in', 
+  '/sign-up',
+  '/category/formal',
+  '/category/casual'
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth.protect()
+  }
 })
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
-
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
